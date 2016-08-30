60d989748f55881e570cb210e29d683beb12ef4c
kschlesi <kschlesi42@gmail.com>
2016-08-29 19:10:50 -0700

    wrote draft loop, need to finish saving

--- evac_grp_ind_crossval.m
+++ evac_grp_ind_crossval.m
@@ -11,102 +11,106 @@ train_trials = trial_ix('ind',ss,1,missing);
 %test_trials = train_trials(loo_ix);
 %train_trials = removeval(train_trials,test_trials);
 
+makeFigs = true;
+legend1 = false;
+
 % for cross-validation on group trials:
-groupProtocol = 'mR';
-groupSize = 25;
-test_trials = trial_ix(groupProtocol,ss,groupSize,missing);
-switch groupProtocol
+gPs = ['fTG','lTG','mR'];
+gSs = [5,25];
+
+% loop over group sizes and protocols
+for groupProtocol = gPs
+  for groupSize = gSs
+
+    % set test trials and protocol strings
+    test_trials = trial_ix(groupProtocol,ss,groupSize,missing);
+    switch groupProtocol
                 case 'fTG',
                   gpstr = 'FTG';
                 case 'mR'
                   gpstr = 'MV';
                 case 'lTG'
                   gpstr = 'LTG';
-end
+    end
 
-makeFigs = true;
-legend1 = false;
+    % load necessary data
+    bins = 0:0.1:1.1;   % this rounds DOWN
+    [Htrain,Jtrain,Theta,AvgCumEvac,missing,Q1,T1,P1,C1] = load_evac_data(0,train_trials,bins);
+    [Htest,Jtest] = load_evac_data(0,test_trials,bins);
+    [ntrials,nts] = size(Q1);
+    [N,~] = size(P1);
 
-%% load necessary data
+    trEnd = zeros(1,size(Q1,1)); % we will store the time at which the trials ended
+    for j = 1:size(Q1,1) % iterate through each trial
+        trEnd(j) = find(Q1(j,:)==Q1(j,end),1,'first');
+    end
 
-%bins = -0.05:0.1:1.05;  % this gives the bin EDGES for Phit values
-bins = 0:0.1:1.1;   % this rounds DOWN
-[Htrain,Jtrain,Theta,AvgCumEvac,missing,Q1,T1,P1,C1] = load_evac_data(0,train_trials,bins);
-[Htest,Jtest] = load_evac_data(0,test_trials,bins);
-[ntrials,nts] = size(Q1);
-[N,~] = size(P1);
+    % first, perform (individual) fit on training trials with MLE
 
-trEnd = zeros(1,size(Q1,1)); % we will store the time at which the trials ended
-for j = 1:size(Q1,1) % iterate through each trial
-    trEnd(j) = find(Q1(j,:)==Q1(j,end),1,'first');
-end
+    % qform = @(Phit_,pv_) pv_(1).*(Phit_.^pv_(3))./(pv_(2).^pv_(3)+Phit_.^pv_(3));
+    % Phit = 0:0.1:1;
+    % qfit = @(pv_) qform(Phit(:,2:end),pv_);
+    % MLfit = @(pvec) -1*sum(((sum(H(:,2:end))-sum(J(:,2:end))).*log(1-qfit(pvec)) + sum(J(:,2:end)).*log(qfit(pvec))));
+    % startp = [1;0.5;10];
 
-%% first, perform (individual) fit on training trials
-% with maximum likelihood estimation
+    qform = @(Phit_,pv_) pv_(1).*Phit_.^pv_(2);
+    Phit = 0:0.1:1;
+    qfit = @(pv_) qform(Phit(:,2:end-1),pv_);
+    MLfit = @(pvec_) -1*sum(((sum(Htrain(:,2:end-1))-sum(Jtrain(:,2:end-1))).*log(1-qfit(pvec_)) + sum(Jtrain(:,2:end-1)).*log(qfit(pvec_))));
+    startp = [1.1;1];
+    options = optimoptions(@fminunc,'MaxFunEvals',10000);
+    [params1,MLval] = fminunc(MLfit,startp,options);
+    A = zeros(length(startp));
+    b = zeros(length(startp),1);
+    % constrains all prob. as a function of Phit to be <=1
+    q_con = @(pvec_)(qform(Phit,pvec_)-1); 
+    [theta_con,MLval_con] = fmincon(MLfit,startp,A,b,A,b,0,100,@(pvec_)q_add_eq(pvec_,q_con));
 
-% qform = @(Phit_,pv_) pv_(1).*(Phit_.^pv_(3))./(pv_(2).^pv_(3)+Phit_.^pv_(3));
-% Phit = 0:0.1:1;
-% qfit = @(pv_) qform(Phit(:,2:end),pv_);
-% MLfit = @(pvec) -1*sum(((sum(H(:,2:end))-sum(J(:,2:end))).*log(1-qfit(pvec)) + sum(J(:,2:end)).*log(qfit(pvec))));
-% startp = [1;0.5;10];
+    if MLval<MLval_con && all(qform(0:0.1:1,params1)<=1)
+        params = params1;
+    else
+        params = theta_con;
+    end
+    
+% MAKE bestfit FIGURE if makeFigs = true
+    if makeFigs
+        figure(1); hold all; plot(Phit,qform(Phit,params));
+        xlabel('Phit'); ylabel('evacuation probability'); title('best-fit hill model, ind. trials, shelterSpace=50');
+        %legend(['\Lambda = ' num2str(params(1)) ', k = ' num2str(params(2)) ', n = ' num2str(params(3))]);
+        legend(['\alpha = ' num2str(params(1)) ', \beta = ' num2str(params(2))]);
+        hold off;
+    end
 
-qform = @(Phit_,pv_) pv_(1).*Phit_.^pv_(2);
-Phit = 0:0.1:1;
-qfit = @(pv_) qform(Phit(:,2:end-1),pv_);
-MLfit = @(pvec_) -1*sum(((sum(Htrain(:,2:end-1))-sum(Jtrain(:,2:end-1))).*log(1-qfit(pvec_)) + sum(Jtrain(:,2:end-1)).*log(qfit(pvec_))));
-startp = [1.1;1];
-options = optimoptions(@fminunc,'MaxFunEvals',10000);
-[params1,MLval] = fminunc(MLfit,startp,options);
-A = zeros(length(startp));
-b = zeros(length(startp),1);
-% constrains all prob. as a function of Phit to be <=1
-q_con = @(pvec_)(qform(Phit,pvec_)-1); 
-[theta_con,MLval_con] = fmincon(MLfit,startp,A,b,A,b,0,100,@(pvec_)q_add_eq(pvec_,q_con));
+% Given training params, perform test on holdout trials
+    Phit = 0:0.1:1;
+    q = @(Phit_) qform(Phit_,params);              % model with params to test
+    tot_err = zeros(numel(test_trials),1);
+    ts_err = zeros(numel(test_trials),nts);
+    %loop over test trials
+    for tr=test_trials';
+    disp(['test trial ' num2str(tr)]);
+    [T, P] = solve_master_binom_ss(N,q,Q1(tr,:),Phit,'ss',ss);
+    Cexp = sum(bsxfun(@times,P,0:1:N),2);
+    tbins = 0:1:nts-1;
+    Cix = zeros(numel(tbins),1);
+    for tx=tbins
+      try  
+        Cix(tbins==tx) = find(T>=tx,1,'first');
+      catch
+        Cix(tbins==tx) = Cix(find(tbins==tx)-1);
+      end
+    end
+    tot_err(test_trials==tr) = Cexp(trEnd(tr)) - C1(tr,trEnd(tr));
+    ts_err(test_trials==tr,1:trEnd(tr)) = Cexp(Cix(1:trEnd(tr)))' - C1(tr,1:trEnd(tr));
 
-if MLval<MLval_con && all(qform(0:0.1:1,params1)<=1)
-    params = params1;
-else
-    params = theta_con;
-end
+    err_pos = (sum(cumsum(P')<0.95));
+    err_neg = (sum(cumsum(P')<0.05));
+    stdevs = [err_pos-Cexp';Cexp'-err_neg];
 
-if makeFigs
-figure(1); hold all; plot(Phit,qform(Phit,params));
-xlabel('Phit'); ylabel('evacuation probability'); title('best-fit hill model, ind. trials, shelterSpace=50');
-%legend(['\Lambda = ' num2str(params(1)) ', k = ' num2str(params(2)) ', n = ' num2str(params(3))]);
-legend(['\alpha = ' num2str(params(1)) ', \beta = ' num2str(params(2))]);
-hold off;
-end
-
-%% Given training params, perform test on holdout trials
-
-Phit = 0:0.1:1;
-q = @(Phit_) qform(Phit_,params);              % model with params to test
-tot_err = zeros(numel(test_trials),1);
-ts_err = zeros(numel(test_trials),nts);
-%loop over test trials
-for tr=test_trials';
-disp(['test trial ' num2str(tr)]);
-[T, P] = solve_master_binom_ss(N,q,Q1(tr,:),Phit,'ss',ss);
-Cexp = sum(bsxfun(@times,P,0:1:N),2);
-tbins = 0:1:nts-1;
-Cix = zeros(numel(tbins),1);
-for tx=tbins
-  try  
-    Cix(tbins==tx) = find(T>=tx,1,'first');
-  catch
-    Cix(tbins==tx) = Cix(find(tbins==tx)-1);
-  end
-end
-tot_err(test_trials==tr) = Cexp(trEnd(tr)) - C1(tr,trEnd(tr));
-ts_err(test_trials==tr,1:trEnd(tr)) = Cexp(Cix(1:trEnd(tr)))' - C1(tr,1:trEnd(tr));
-
-err_pos = (sum(cumsum(P')<0.95));
-err_neg = (sum(cumsum(P')<0.05));
-stdevs = [err_pos-Cexp';Cexp'-err_neg];
-
+% MAKE comparison FIGURE if makeFigs = true    
 if makeFigs    
     if legend1
-figure; %bcolor(P',T,0:1:N); colorbar; hold all; % probability distribution
+        figure; %bcolor(P',T,0:1:N); colorbar; hold all; % probability distribution
         plot(T,Cexp,'k','LineWidth',2); hold all;  % expected value
         plot(tbins,C1(tr,:),'color',[0 0.5 0],'LineWidth',2);                         % empirical value
         plot(tbins,N*(floor((Q1(tr,:))*10)/10),'--','LineWidth',1);                 % Phit value
@@ -216,13 +220,20 @@ figure(2); hold all;
         %suptitle(['Shelter Capacity ' num2str(ss) ', Groups of ' num2str(groupSize)]);
         title(trial_conv(tr,ss,groupSize,groupProtocol));
         hold off; 
-end
+end  % end makeFigs
 
-end
+    end  % end loop over test trials
 
-RMSEs = rmse(ts_err');
- % LOOP OVER GROUP SIZE AND protocol?
+% SAVE the relevant errors
+savingstr = [lower(gpstr),num2str(groupSize)];
+eval(['rmse_' savingstr ' = rmse(ts_err'');']);
+eval(['tse_' savingstr ' = ts_err;']);
+save_vars = save_vars
 
+  end  % end loop over group size
+end  % end loop over protocol
+
+save(['ind_grp_crossval_ss' num2str(ss) 't.mat'],'');
 
 %% MAKE SUMMARY PLOTS
 
